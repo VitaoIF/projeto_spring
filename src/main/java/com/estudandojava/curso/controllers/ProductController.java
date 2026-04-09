c8b5a73d94a6c59d4e2476183fc7d1121e57adda
VitaoIF <victormelo1212@gmail.com>
2026-04-09 19:51:24 -0300

    Feat: adiciona insercao, delecao e atualizacao ao controller de produtos

--- src/main/java/com/estudandojava/curso/controllers/ProductController.java
+++ src/main/java/com/estudandojava/curso/controllers/ProductController.java
@@ -4,11 +4,10 @@ import com.estudandojava.curso.entities.Product;
 import com.estudandojava.curso.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,6 +26,28 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Product> findById(@PathVariable Long id){
         Product product = service.findById(id);
+        return ResponseEntity.ok().body(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> insert(@RequestBody Product product){
+        product = service.insert(product);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(product.getId()).toUri();
+        return ResponseEntity.created(uri).body(product);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product){
+        product = service.update(id, product);
         return ResponseEntity.ok().body(product);
     }
 
