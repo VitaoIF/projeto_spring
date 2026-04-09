2e47f358cb9188e03ad9434d655c459528582add
VitaoIF <victormelo1212@gmail.com>
2026-04-09 19:45:58 -0300

    Feat: adiciona insercao, delecao e atualizacao ao controller de categorias

--- src/main/java/com/estudandojava/curso/controllers/CategoryController.java
+++ src/main/java/com/estudandojava/curso/controllers/CategoryController.java
@@ -4,10 +4,10 @@ import com.estudandojava.curso.entities.Category;
 import com.estudandojava.curso.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,8 +24,30 @@ public class CategoryController {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Category> findById(Long id){
+    public ResponseEntity<Category> findById(@PathVariable Long id){
         Category category = service.findById(id);
+        return ResponseEntity.ok().body(category);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> insert(@RequestBody Category category){
+        category = service.insert(category);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(category.getId()).toUri();
+        return ResponseEntity.created(uri).body(category);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category category){
+        category = service.update(id, category);
         return ResponseEntity.ok().body(category);
     }
 }
