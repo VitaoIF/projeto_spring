00a65fc60f1571678c55abea2f3c4374769a7660
VitaoIF <victormelo1212@gmail.com>
2026-04-09 19:57:51 -0300

    Feat: adiciona insercao, delecao e atualizacao ao service de pedido

--- src/main/java/com/estudandojava/curso/services/OrderService.java
+++ src/main/java/com/estudandojava/curso/services/OrderService.java
@@ -2,7 +2,11 @@ package com.estudandojava.curso.services;
 
 import com.estudandojava.curso.entities.Order;
 import com.estudandojava.curso.repositories.OrderRepository;
+import com.estudandojava.curso.services.exceptions.DatabaseException;
+import com.estudandojava.curso.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +24,32 @@ public class OrderService {
 
     public Order findById(Long id){
         Optional<Order> orderId = repository.findById(id);
-        return orderId.get();
+        return orderId.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Order insert(Order order){
+        return repository.save(order);
+    }
+
+    public void delete(Long id){
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Order update(Long id, Order order){
+        Order entity = repository.getReferenceById(id);
+        updateOrder(entity, order);
+        return repository.save(entity);
+    }
+
+    private void updateOrder(Order entity, Order order) {
+        entity.setOrderStatus(order.getOrderStatus());
+        entity.setPayment(order.getPayment());
+        entity.setNow(order.getNow());
     }
 }
