4b93c478b4c6fb6a60bd18825c93511c871c3280
VitaoIF <victormelo1212@gmail.com>
2026-04-09 20:04:33 -0300

    Feat: adiciona Delecao, insercao e atualizacao no controller de order

--- src/main/java/com/estudandojava/curso/controllers/OrderController.java
+++ src/main/java/com/estudandojava/curso/controllers/OrderController.java
@@ -4,11 +4,10 @@ import com.estudandojava.curso.entities.Order;
 import com.estudandojava.curso.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,4 +28,24 @@ public class OrderController {
         Order order = service.findById(id);
         return ResponseEntity.ok().body(order);
     }
+
+    @PostMapping
+    public ResponseEntity<Order> insert(Order order){
+        order = service.insert(order);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(order.getId()).toUri();
+        return ResponseEntity.created(uri).body(order);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Order> update(@PathVariable Long id, @RequestBody Order order){
+        order = service.update(id, order);
+        return ResponseEntity.ok().body(order);
+    }
+
 }
