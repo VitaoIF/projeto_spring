c36570730b59192983b68145704ada3c026e5ffb
VitaoIF <victormelo1212@gmail.com>
2026-04-09 19:19:05 -0300

    Feat: adiciona insercao, delecao e atualizacao ao service de produtos

--- src/main/java/com/estudandojava/curso/services/ProductService.java
+++ src/main/java/com/estudandojava/curso/services/ProductService.java
@@ -2,7 +2,12 @@ package com.estudandojava.curso.services;
 
 import com.estudandojava.curso.entities.Product;
 import com.estudandojava.curso.repositories.ProductRepository;
+import com.estudandojava.curso.services.exceptions.DatabaseException;
+import com.estudandojava.curso.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,7 +25,38 @@ public class ProductService {
 
     public Product findById(Long id){
         Optional<Product> product = productRepository.findById(id);
-        return product.get();
+        return product.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Product insert(Product product){
+        return productRepository.save(product);
+    }
+
+    public void delete(Long id){
+        try {
+            productRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Product update(Long id, Product product){
+        try {
+            Product entity = productRepository.getReferenceById(id);
+            updateProduct(entity, product);
+            return productRepository.save(product);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateProduct(Product entity, Product product) {
+        entity.setName(product.getName());
+        entity.setDescription(product.getDescription());
+        entity.setPrice(product.getPrice());
+        entity.setImgUrl(product.getImgUrl());
     }
 
 }
